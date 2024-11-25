bdfb3f740e19b5c64e4dd8827f8dd23f78b597a9
Takahito Urai <uraitakahito@gmail.com>
2024-11-26 02:30:20 +0900

    Remove unused files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-const greeting_1 = require("./greeting");
-const log = (...args) => {
-    console.log(...args);
-};
-exports.log = log;
-(0, exports.log)((0, greeting_1.hello)("unbuild"));
